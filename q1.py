3a8ab4bd83453569d8187a870bd4b981c50e518d
RexYing <rex.ying0923@gmail.com>
2017-05-25 00:12:46 -0700

    rmax

--- q1.py
+++ q1.py
@@ -4,54 +4,87 @@ from frozen_lake import *
 import numpy as np
 import time
 from utils import *
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 
 def rmax(env, gamma, m, R_max, epsilon, num_episodes, max_step = 6):
-	"""Learn state-action values using the Rmax algorithm
+    """Learn state-action values using the Rmax algorithm
+    
+    Args:
+    ----------
+    env: gym.core.Environment
+    	Environment to compute Q function for. Must have nS, nA, and P as
+    	attributes.
+    m: int
+    	Threshold of visitance
+    R_max: float 
+    	The estimated max reward that could be obtained in the game
+    epsilon: 
+    	accuracy paramter
+    num_episodes: int 
+    	Number of episodes of training.
+    max_step: Int
+    	max number of steps in each episode
+    
+    Returns
+    -------
+    np.array
+      An array of shape [env.nS x env.nA] representing state-action values
+    """
+    
+    Q = np.ones((env.nS, env.nA)) * R_max / (1 - gamma)
+    R = np.zeros((env.nS, env.nA))
+    nSA = np.zeros((env.nS, env.nA))
+    nSASP = np.zeros((env.nS, env.nA, env.nS))
+    ########################################################
+    #                   YOUR CODE HERE                     #
+    ########################################################
 
-	Args:
-	----------
-	env: gym.core.Environment
-		Environment to compute Q function for. Must have nS, nA, and P as
-		attributes.
-	m: int
-		Threshold of visitance
-	R_max: float 
-		The estimated max reward that could be obtained in the game
-	epsilon: 
-		accuracy paramter
-	num_episodes: int 
-		Number of episodes of training.
-	max_step: Int
-		max number of steps in each episode
+    avg_scores = np.zeros(num_episodes)
+    for episode in range(num_episodes):
+        s = env.reset()
+        done = False
+        t = 0 # step
+        total_score = 0
+        while not done and t < max_step:
+            best_a = np.argmax(Q[s])
+            s_next, r, done, _ = env.step(best_a)
+            total_score += r
+            if nSA[s][best_a] < m:
+                nSA[s][best_a] += 1
+                R[s][best_a] += r
+                nSASP[s][best_a][s_next] += 1
+                if nSA[s][best_a] == m:              # update all Q functions
+                    for i in range(int( np.ceil(np.log(1 / (epsilon * (1 - gamma))) / (1 - gamma)) )):
+                        for s1 in range(env.nS):
+                            for a1 in range(env.nA):
+                                if nSA[s1][a1] >= m: # update Q function
+                                    q = R[s1][a1] / nSA[s1][a1]
+                                    for s2 in range(env.nS):
+                                        transition = nSASP[s1][a1][s2] / nSA[s1][a1]
+                                        q += gamma * transition * np.max(Q[s2])
+                                    Q[s1][a1] = q
+            s = s_next 
+            t += 1
+        avg_scores[episode] = total_score / (episode + 1)
 
-	Returns
-	-------
-	np.array
-	  An array of shape [env.nS x env.nA] representing state-action values
-	"""
+    plt.plot(range(num_episodes), avg_scores)
+    plt.savefig('avg_scores_m=' + str(m) + '.png')
 
-	Q = np.ones((env.nS, env.nA)) * R_max / (1 - gamma)
-	R = np.zeros((env.nS, env.nA))
-	nSA = np.zeros((env.nS, env.nA))
-	nSASP = np.zeros((env.nS, env.nA, env.nS))
-	########################################################
-	#                   YOUR CODE HERE                     #
-	########################################################
-
-	
-
+            
 
 	########################################################
 	#                    END YOUR CODE                     #
 	########################################################
-	return Q
+    return Q
 
 
 def main():
 	env = FrozenLakeEnv(is_slippery=False)
 	print env.__doc__
-	Q = rmax(env, gamma = 0.99, m=10, R_max = 1, epsilon = 0.1, num_episodes = 1000)
+	Q = rmax(env, gamma = 0.99, m=10, R_max = 1, epsilon = 0.1, num_episodes = 10000)
 	render_single_Q(env, Q)
 
 
